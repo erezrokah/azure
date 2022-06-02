18e9bd6db9e57715ca5cb8ba7de52c74672b614d
Erez Rokah <erezrokah@users.noreply.github.com>
2022-06-02 18:40:23 +0300

    fix: Support postgres:10 (#48)

--- queries/compute/internet-facing_virtual_machines_should_be_protected_with_network_security_groups.sql
+++ queries/compute/internet-facing_virtual_machines_should_be_protected_with_network_security_groups.sql
@@ -2,7 +2,7 @@ SELECT vm.id
 FROM
   azure_compute_virtual_machines vm
   LEFT OUTER JOIN azure_security_assessments a
-  ON a.name = '483f12ed-ae23-447e-a2de-a67a10db4353' AND starts_with(a.id, vm.id || '/') 
+  ON a.name = '483f12ed-ae23-447e-a2de-a67a10db4353' AND a.id like (vm.id || '/' || '%')
 WHERE
   a.name IS NULL
   OR (
--- queries/web/apps_with_logging_disabled.sql
+++ queries/web/apps_with_logging_disabled.sql
@@ -1,6 +1,6 @@
 SELECT subscription_id,
        id
 FROM azure_web_apps a
-WHERE NOT (a.site_config -> 'httpLoggingEnabled')::bool
-   OR NOT (a.site_config -> 'detailedErrorLoggingEnabled')::bool
-   OR NOT (a.site_config -> 'requestTracingEnabled')::bool;
+WHERE NOT (a.site_config -> 'httpLoggingEnabled')::text::bool
+   OR NOT (a.site_config -> 'detailedErrorLoggingEnabled')::text::bool
+   OR NOT (a.site_config -> 'requestTracingEnabled')::text::bool;
